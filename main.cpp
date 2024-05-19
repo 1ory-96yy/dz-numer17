1a6948a45334ed817e3c23a61839328f55960b2e
Artur <arturvasylkivsky@gmail.com>
2024-05-19 13:08:49 +0300

    qqwertyu

---
+++ main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+
+template<typename T>
+class TemplateQueue {
+private:
+    struct Node {
+        T data;
+        Node* next;
+        Node(const T& val) : data(val), next(nullptr) {}
+    };
+
+    Node* front;
+    Node* rear;
+
+public:
+    TemplateQueue() : front(nullptr), rear(nullptr) {}
+    void enqueue(const T& val) {
+        Node* newNode = new Node(val);
+        if (rear == nullptr) {
+            front = rear = newNode;
+        }
+        else {
+            rear->next = newNode;
+            rear = newNode;
+        }
+    }
+    void dequeue() {
+        if (isEmpty()) {
+            std::cerr << "Queue is empty, cannot dequeue.\n";
+        }
+        else {
+            Node* temp = front;
+            front = front->next;
+            delete temp;
+            if (front == nullptr) {
+                rear = nullptr;
+            }
+        }
+    }
+
+
+    bool isEmpty() const {
+        return front == nullptr;
+    }
+
+
+    T peek() const {
+        if (isEmpty()) {
+            std::cerr << "Queue is empty.\n";
+            throw std::out_of_range("Queue is empty.");
+        }
+        else {
+            return front->data;
+        }
+    }
+    void show() const {
+        Node* temp = front;
+        std::cout << "Queue elements: ";
+        while (temp != nullptr) {
+            std::cout << temp->data << " ";
+            temp = temp->next;
+        }
+        std::cout << std::endl;
+    }
+    ~TemplateQueue() {
+        while (!isEmpty()) {
+            dequeue();
+        }
+    }
+};
+
+int main() {
+    TemplateQueue<int> intQueue;
+    intQueue.enqueue(10);
+    intQueue.enqueue(20);
+    intQueue.enqueue(30);
+    intQueue.show();
+    std::cout << "Peek: " << intQueue.peek() << std::endl;
+    intQueue.dequeue();
+    intQueue.show();
+    TemplateQueue<double> doubleQueue;
+    doubleQueue.enqueue(3.14);
+    doubleQueue.enqueue(2.718);
+    doubleQueue.show();
+    std::cout << "Peek: " << doubleQueue.peek() << std::endl;
+
+    return 0;
+}
